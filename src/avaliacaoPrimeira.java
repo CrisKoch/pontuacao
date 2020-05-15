77a5addcb881887f652b503fcf3a3b530e83cf6c
CrisKoch <cristinak.iftm@gmail.com>
2020-05-15 19:20:54 -0300

    Cabecalho

--- src/avaliacaoPrimeira.java
+++ src/avaliacaoPrimeira.java
@@ -1,9 +1,33 @@
-
+import java.util.Locale;
+import java.util.Scanner;
 public class avaliacaoPrimeira {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		System.out.println("SISTEMA DE PERFIL DE CLIENTE");
+		System.out.println("------------------------------");
+		System.out.println("INFORME OS DADOS DO ÚLTIMO ANO");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
 	}
 
 }
