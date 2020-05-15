6a632e9c2952a68168598db3b2976dd9756da276
CrisKoch <cristinak.iftm@gmail.com>
2020-05-15 19:33:39 -0300

    Leitura do volume

--- src/avaliacaoPrimeira.java
+++ src/avaliacaoPrimeira.java
@@ -6,11 +6,23 @@ public class avaliacaoPrimeira {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
+		int quantidade;
+		double ticketMedio;
 		
+		
+		//Cabecalho
 		
 		System.out.println("SISTEMA DE PERFIL DE CLIENTE");
 		System.out.println("------------------------------");
 		System.out.println("INFORME OS DADOS DO ÚLTIMO ANO");
+		
+		//Leitura do volume
+		
+		System.out.println();
+		System.out.print("Quantas compras o cliente fez no último ano? ");
+		quantidade = sc.nextInt();
+		System.out.print("Qual o ticket médio? ");
+		ticketMedio = sc.nextDouble();
 		
 		
 		
